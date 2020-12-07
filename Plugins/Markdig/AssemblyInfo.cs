4bce05b12dcee7bd39c9fa4fef435f518813ab74
R. Beltran <rbeltran8000@gmail.com>
2020-12-08 00:46:37 +0100

    first commit

---
+++ Plugins/Markdig/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using UnityEditor.UIElements;
+
+[assembly: AssemblyTitle("Markdown.Markdig")]
+[assembly: AssemblyProduct("com.markdown.markdig")]
+[assembly: AssemblyCopyright("MIT License")]
