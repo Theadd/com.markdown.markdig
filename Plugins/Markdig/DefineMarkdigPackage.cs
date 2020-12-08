a0a698ea8c26d8b362f6aa62461e9d02ce77e44c
R. Beltran <rbeltran8000@gmail.com>
2020-12-08 02:37:28 +0100

    add MARKDIG_PACKAGE in player's scripting define symbols on load

---
+++ Plugins/Markdig/DefineMarkdigPackage.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+
+namespace Markdown.Markdig
+{
+    using System;
+    using System.Linq;
+    using UnityEditor;
+
+    public static class DefineMarkdigPackage
+    {
+        public  static readonly string[] DEFINES = new string[] { "MARKDIG_PACKAGE" };
+
+        [InitializeOnLoadMethod]
+        private static void SetScriptingDefineSymbol()
+        {
+            var currentTarget = EditorUserBuildSettings.selectedBuildTargetGroup;
+            
+            if (currentTarget == BuildTargetGroup.Unknown)
+                return;
+
+            var definesString = PlayerSettings.GetScriptingDefineSymbolsForGroup(currentTarget).Trim();
+            var defines = definesString.Split(';');
+
+            bool changed = false;
+
+            foreach (var define in DEFINES)
+            {
+                if (!defines.Contains(define))
+                {
+                    if (!definesString.EndsWith(";", StringComparison.InvariantCulture))
+                        definesString += ";";
+
+                    definesString += define;
+                    changed = true;
+                }
+            }
+
+            if (changed)
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(currentTarget, definesString);
+            }
+        }
+    }
+}
+
+#endif
